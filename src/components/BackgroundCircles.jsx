410eec2e34902a26e750ab1369181cef13309ca4
Pete <pete.zink03@gmail.com>
2025-02-05 09:26:02 -0500

    Updated Hero Section. Added icons absolutely positioned to circle the hero image.

---
+++ src/components/BackgroundCircles.jsx
@@ -0,0 +1,79 @@
+import { MouseParallax } from "react-just-parallax";
+import javascript from "../assets/javascript.png"
+import html from "../assets/html.png"
+import css from "../assets/css.png"
+// import appwrite from "../assets/appwrite.png"
+import react from "../assets/react.png"
+// import python from "../assets/Python.png"
+// import github from "../assets/github.png"
+import tailwind from "../assets/tailwind.png"
+// import postgresql from "../assets/postgresql.png"
+import typescript from "../assets/Typescriptlogo.png"
+// import django from "../assets/Django.png"
+// import bootstrap from "../assets/Bootstrap.png"
+// import docker from "../assets/Docker.png"
+// import fastapi from "../assets/fastapi.png"
+
+const Rings = () => {
+    return (
+      <>
+        <div className="absolute top-1/2 left-1/2 w-[65.875rem] aspect-square border border-gray-900 rounded-full -translate-x-1/2 -translate-y-1/2" />
+        <div className="absolute top-1/2 left-1/2 w-[51.375rem] aspect-square border border-gray-900 rounded-full -translate-x-1/2 -translate-y-1/2" />
+        <div className="absolute top-1/2 left-1/2 w-[36.125rem] aspect-square border border-gray-900 rounded-full -translate-x-1/2 -translate-y-1/2" />
+        <div className="absolute top-1/2 left-1/2 w-[23.125rem] aspect-square border border-gray-900 rounded-full -translate-x-1/2 -translate-y-1/2" />
+      </>
+    );
+  };
+
+export const BackgroundCircles = ({ parallaxRef }) => {
+
+    return (
+      <div className="absolute -top-[42.375rem] left-1/2 w-[78rem] aspect-square border border-gray-900 rounded-full -translate-x-1/2 md:-top-[38.5rem] xl:-top-[2rem]">
+        <Rings />
+        <MouseParallax strength={0.07} parallaxContainerRef={parallaxRef}>
+          <div className="absolute bottom-1/2 left-1/2 w-0.25 h-1/2 origin-bottom rotate-[36deg] ">
+            <img
+            src={javascript}
+            className="w-12 h-12 rounded-lg -rotate-[36deg]"
+            alt="javascript"
+            />
+          </div>
+          <div className="absolute bottom-1/2 left-1/2 w-0.25 h-1/2 origin-bottom -rotate-[36deg]">
+            <img
+            src={typescript}
+            className="w-12 h-12 rounded-lg mt-2 rotate-[36deg]"
+            alt="typescript"
+            />
+          </div>
+          <div className="absolute bottom-1/2 left-1/2 w-0.25 h-1/2 origin-bottom -rotate-[65deg]">
+            <img
+            src={react}
+            className="w-12 h-12 rounded-lg"
+            alt="react"
+            />
+          </div>
+          <div className="absolute bottom-1/2 left-1/2 w-0.25 h-1/2 origin-bottom -rotate-[95deg]">
+            <img
+            src={tailwind}
+            className="w-12 h-12 rounded-lg rotate-[75deg]"
+            alt="tailwind"
+            />
+          </div>
+          <div className="absolute bottom-1/2 left-1/2 w-0.25 h-1/2 origin-bottom rotate-[95deg]">
+            <img
+            src={html}
+            className="w-12 h-12 rounded-lg mt-24 mr-24 -rotate-[95deg]"
+            alt="html"
+            />
+          </div>
+          <div className="absolute bottom-1/2 left-1/2 w-0.25 h-1/2 origin-bottom rotate-[70deg]">
+            <img
+            src={css}
+            className="w-12 h-12 rounded-lg mt-44 mr-64 -rotate-[70deg]"
+            alt="css"
+            />
+          </div>
+        </MouseParallax>
+      </div>
+    );
+  };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,42 +1,52 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import HeroImage from '../assets/Gradpic.jpg'
 import {MdKeyboardArrowRight} from 'react-icons/md'
 import {Link} from 'react-scroll'
 import Typewriter from "./Typewrite.jsx";
+import { BackgroundCircles } from './BackgroundCircles.jsx';
 
 
 const Home = () => {
+    const parallaxRef = useRef(null)
+
   return (
-    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 '>
-        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
-            <div className='flex flex-col justify-center h-full'>
-                <h2 className='text-4xl mt-20 pt-10 font-bold text-blue-500 border-b-4 border-gray-500 pr-4 section-title'>
-                Hi!  <Typewriter text=" I'm Pete Zink" delay={300} />
-                </h2>
-                <p className='text-white py-4 max-w-md '>
-                <Typewriter text="I'm a Fullstack Engineer specializing in frontend development, with expertise in React, JavaScript, Python, CSS and other frameworks. Adept at creating captivating user interfaces that seamlessly integrate interactivity and design. I'm a collaborative team player committed to delivering exceptional web experiences that merge innovation with functionality. Check out some of my projects below!" delay={25} />
-                </p>
-                <div className='flex'>
-                    <Link to='projects' smooth duration={500} className=' mr-4 group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
-                        Projects
-                        <span className='group-hover:rotate-90 duration-300'>
-                        <MdKeyboardArrowRight size={26} className='ml-1' />
-                        </span>
-                    </Link>
-                    <Link to='contact' smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
-                        Contact
-                        <span className='group-hover:rotate-90 duration-300'>
-                        <MdKeyboardArrowRight size={25} className='ml-1' />
-                        </span>
-                    </Link>
+    <div name="home" className='h-screen w-full bg-gradient-to-b  from-black via-black to-gray-800 relative' ref={parallaxRef}>
+        <div className='max-w-screen-lg mx-auto flex flex-col items-center z-10  justify-center h-full px-4 relative'>
+            <div className='flex flex-col justify-center h-full relative '>
+                        <h2 className='text-4xl text-center mt-20 pt-10 font-bold text-blue-500 border-b-4 border-gray-500 section-title'>
+                        Hi!  <Typewriter text=" I'm Pete Zink" delay={300} />
+                        </h2>
+            <div className='flex w-full h-1/2 mt-'>
+                <img src={HeroImage} alt='my profile' className='md:rounded-full rounded-xl w-full mt-12 object-cover md:p-10 mt-10' />
+            </div>
+                    <div className='max-w-xl'>
+                        <p className='text-white py-4'>
+                        <Typewriter text="I'm a Fullstack Engineer specializing in frontend development, with expertise in React, JavaScript, Python, CSS and other frameworks. Adept at creating captivating user interfaces that seamlessly integrate interactivity and design. I'm a collaborative team player committed to delivering exceptional web experiences that merge innovation with functionality. Check out some of my projects below!" delay={25} />
+                        </p>
+                        <div className='flex items-center justify-center'>
+                            <Link to='projects' smooth duration={500} className=' mr-4 group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+                                Projects
+                                <span className='group-hover:rotate-90 duration-300'>
+                                <MdKeyboardArrowRight size={26} className='ml-1' />
+                                </span>
+                            </Link>
+                            <Link to='contact' smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+                                Contact
+                                <span className='group-hover:rotate-90 duration-300'>
+                                <MdKeyboardArrowRight size={25} className='ml-1' />
+                                </span>
+                            </Link>
+
+                        </div>
+
+                    </div>
 
                 </div>
+            </div>
+            <div className='hidden md:flex'>
 
+        <BackgroundCircles />
             </div>
-            <div>
-                <img src={HeroImage} alt='my profile' className='rounded-2xl mx-auto w-2/3 md:w-full mt-6' />
-            </div>
-        </div>
 
     </div>
   )
